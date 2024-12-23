65846511ee6c44b84adec5e64a581bd5d09b5368
Nurlybek <nurlybekoroz@gmail.com>
2024-12-23 19:27:04 +0600

    Создал компонент Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,9 @@
-const Card = (rank = 5, suit = "diams") => {
-  return(
-      <span className="card rank-{rank} diams">
+const Card = (rank = 'k', suit = "diams") => {
+    return(
+        <span className="card rank-k diams">
         <span className="rank">{rank}</span>
         <span className="suit">{suit}</span>
       </span>
-  );
+    );
 };
 export default Card
