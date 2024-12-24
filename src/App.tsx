3fc8756ca7e64e91fb0acf5d9691297d08cda4a7
Nurlybek <nurlybekoroz@gmail.com>
2024-12-24 14:03:00 +0600

    Вывел в компонент App кнопку "Раздать карты"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 import './App.css'
 import CardDeck from "./lib/CardDeck.ts";
 import {useState} from "react";
+import Card from './lib/Card';
+import PockerHand from "./lib/PockerHand.ts";
+
 
 
 
 const App = () => {
-    const [hand, setHand] = useState([])
-    const deck = new CardDeck();
-    console.log(deck.deck);
+    const [hand, setHand] = useState<Card[]>([]);
+    const [deck, setDeck] = useState(new CardDeck());
+    const [handOutcome, setHandOutcome] = useState<string>("");
+    const GiveCards = () => {
+        const CardsGiven = deck.getCards(5);
+        setHand(CardsGiven);
 
+        const outcome = new PockerHand(CardsGiven).getOutcome();
+        setHandOutcome(outcome);
+        setDeck(deck);
+    };
     return (
         <>
+            <div className="count">Card count:{deck.getRemainingCount()}</div>
+            <div className="result">result of round:{handOutcome}</div>
+            <br/>
             <div className="playingCards faceImages">
-     <span className="card rank-{rank} diams">
-        <span className="rank">k</span>
-        <span className="suit">$</span>
-      </span>
+                {hand.map(({rank, suit}) => (
+                    <span className={`card rank-${rank} ${suit}`}>
+                        <span className="rank">{rank}</span>
+                        <span className="suit"></span>
+                    </span>
+                ))}
             </div>
-            <button>Раздать карты</button>
+            <button onClick={GiveCards}>Раздать карты</button>
         </>
     );
 
